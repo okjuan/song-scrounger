3a0b40b00b29d479c0acf137f88c32497adb8377
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2020-04-25 16:35:23 -0700

    Fetch quoted songs from Spotify - Clean song names to avoid false negatives

--- song_scrounger.py
+++ song_scrounger.py
@@ -1,24 +1,40 @@
+import asyncio
+import os
+
 from document_parser import find_quoted_tokens
+from spotify_client import SpotifyClient
 from util import read_file_contents
 
-def scrounge_songs(filename):
-    tokens = find_quoted_tokens(read_file_contents(filename))
+async def create_playlist(input_filename, client_id, secret_key):
+    spotify_client = SpotifyClient(client_id, secret_key)
+    track_names = find_quoted_tokens(read_file_contents(input_filename))
+    track_names = map(lambda track_name: track_name.strip(" ,"), track_names)
+    tracks = await _get_tracks(track_names, spotify_client)
+    print(tracks)
 
-    song_data = [dict(spotify_uri=token) for token in tokens]
+async def _get_tracks(track_names, spotify_client):
+    tracks = []
+    for track_name in track_names:
+        if len(track_name) == 0:
+            print(f"WARN: Skipping empty track name.")
+            continue
 
+        results = await spotify_client.find_track(track_name)
+        track = results[0] if len(results) > 0 else None
+        if track is None:
+            print(f"ERROR: Could not find song with name '{track_name}'")
+            continue
 
-    return song_data
+        tracks.append(track)
+    return tracks
 
-def create_playlist(name, spotify_uris):
-    # TODO
-    print(f"TODO: implement create_playlist for spotify_uris: {spotify_uris}")
+async def main():
+    client_id = os.environ.get("SPOTIFY_CLIENT_ID")
+    secret_key = os.environ.get("SPOTIFY_SECRET_KEY")
+    if client_id is None or secret_key is None:
+        raise Exception("Env vars 'SPOTIFY_CLIENT_ID' and 'SPOTIFY_SECRET_KEY' must be set.")
 
-def main():
-    #spotify_client = SpotifyClient()
-    create_playlist(
-        "Created by Song Scrounger",
-        [song_data.get('spotify_uri') for song_data in scrounge_songs("tests/test.txt")]
-    )
+    await create_playlist("tests/test.txt", client_id, secret_key)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- spotify_client.py
+++ spotify_client.py
@@ -11,7 +11,10 @@ class SpotifyClient:
     async def _get_client(self):
         return spotify.Client(self.client_id, self.secret_key)
 
-    async def find_track(self, track_name : str, verbatim=True):
+    async def find_track(self, track_name: str, verbatim=True):
+        if len(track_name) == 0:
+            raise ValueError("Track name cannot be empty.")
+
         # apparently only the async interface supports context management
         async with spotify.Client(self.client_id, self.secret_key) as cli:
             results = await cli.search(track_name, types=["track"])
