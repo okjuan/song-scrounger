fb75452c77110b64b0c0fc3bcbe452d502268509
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2020-05-10 15:52:53 -0700

    Refactor test into class

--- test_song_scrounger.py
+++ test_song_scrounger.py
@@ -4,10 +4,14 @@ import random
 from song_scrounger import SongScrounger
 
 
-async def test_create_playlist(song_scrounger):
-    name = f"Song Scrounger {random.randint(0,10000)}"
-    playlist = await song_scrounger.create_playlist("test_inputs/test_mini.txt", name)
-    print("Got playlist: ", playlist)
+class TestSongScrounger:
+    def __init__(self, client_id, secret_key, bearer_token):
+        self.song_scrounger = SongScrounger(client_id, secret_key, bearer_token)
+
+    async def test_create_playlist(self):
+        name = f"Song Scrounger {random.randint(0,10000)}"
+        playlist = await self.song_scrounger.create_playlist("test_inputs/test_mini.txt", name)
+        print("Got playlist: ", playlist)
 
 async def main():
     client_id = os.environ.get("SPOTIFY_CLIENT_ID")
@@ -16,8 +20,8 @@ async def main():
     if client_id is None or secret_key is None or bearer_token is None:
         raise Exception("Env vars 'SPOTIFY_CLIENT_ID', 'SPOTIFY_SECRET_KEY', 'SPOTIFY_BEARER_TOKEN' must be set.")
 
-    song_scrounger = SongScrounger(client_id, secret_key, bearer_token)
-    await test_create_playlist(song_scrounger)
+    tests = TestSongScrounger(client_id, secret_key, bearer_token)
+    await tests.test_create_playlist()
 
 
 if __name__ == "__main__":
