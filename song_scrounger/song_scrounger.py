c83e4cb06cfad9b95aba6465e840bbf09a974958
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2020-05-16 17:48:54 -0700

    Mock dependencies in song_scrounger tests

--- song_scrounger/song_scrounger.py
+++ song_scrounger/song_scrounger.py
@@ -8,18 +8,12 @@ from .util import read_file_contents, get_spotify_creds, get_spotify_bearer_toke
 
 
 class SongScrounger:
-    def __init__(self, client_id, secret_key, bearer_token=None):
-        self.bearer_token_is_set = not bearer_token is None
-        self.spotify_client = SpotifyClient(client_id, secret_key, bearer_token)
-
-    def fail_if_no_bearer_token(self, reason):
-        if not self.bearer_token_is_set:
-            raise Exception(f"bearer_token is None: {reason}.")
+    def __init__(self, spotify_client):
+        self.spotify_client = spotify_client
 
     async def create_playlist(self, input_filename, playlist_name):
-        self.fail_if_no_bearer_token("need Bearer Token to create playlist")
-        track_names = find_quoted_tokens(read_file_contents(input_filename))
-        track_names = map(lambda track_name: track_name.strip(" ,"), track_names)
+        file_contents = read_file_contents(input_filename)
+        track_names = self.parse_tracks(file_contents)
         tracks = await self._get_tracks(track_names)
         playlist = await self.spotify_client.create_playlist(playlist_name)
         return await self.spotify_client.add_tracks(playlist, tracks)
@@ -40,10 +34,21 @@ class SongScrounger:
             tracks.append(track)
         return tracks
 
+    def parse_tracks(self, corpus):
+        """
+        Params:
+            corpus (str): text in which to search for track names.
+
+        Returns:
+            (Iterator of str): sequence of track names e.g. ["Redbone", "Hotline Bling"].
+        """
+        track_names = find_quoted_tokens(corpus)
+        return map(lambda track_name: track_name.strip(" ,"), track_names)
+
 async def main(input_file, playlist_name):
     client_id, secret_key = get_spotify_creds()
     bearer_token = get_spotify_bearer_token()
-    song_scrounger = SongScrounger(client_id, secret_key, bearer_token=bearer_token)
+    song_scrounger = SongScrounger(SpotifyClient(client_id, secret_key, bearer_token))
     await song_scrounger.create_playlist(input_file, playlist_name)
 
 if __name__ == "__main__":
--- tests/test_song_scrounger.py
+++ tests/test_song_scrounger.py
@@ -3,36 +3,62 @@ import os
 import random
 import unittest
 
+from unittest.mock import AsyncMock, MagicMock, patch
+
 from song_scrounger.song_scrounger import SongScrounger
-from song_scrounger.util import get_spotify_creds, get_spotify_bearer_token
 
 
 class TestSongScrounger(unittest.IsolatedAsyncioTestCase):
-    @classmethod
-    def setUpClass(cls):
-        client_id, secret_key = get_spotify_creds()
-        bearer_token = get_spotify_bearer_token()
-        cls.song_scrounger = SongScrounger(client_id, secret_key, bearer_token)
+    async def asyncSetUp(self):
+        self.mock_spotify_client = AsyncMock()
+        self.song_scrounger = SongScrounger(self.mock_spotify_client)
 
     @classmethod
     def _get_path_to_test_input_file(cls, name):
         # Relative from repo root
         return os.path.abspath(f"tests/test_inputs/{name}")
 
-    async def test_create_playlist(self):
-        name = f"Song Scrounger {random.randint(0,10000)}"
-        playlist = await self.song_scrounger.create_playlist(
-            TestSongScrounger._get_path_to_test_input_file("test_mini.txt"), name)
+    @patch("song_scrounger.song_scrounger.read_file_contents", return_value="Mock corpus with \"Mock Track Name\"")
+    @patch("song_scrounger.song_scrounger.find_quoted_tokens", return_value=["mock token"])
+    async def test_create_playlist(self, mock_find_quoted_tokens, mock_read_file_contents):
+        mock_spotify_track = MagicMock(name="Mock Spotify Track")
+        self.mock_spotify_client.configure_mock(**{
+            "find_track.return_value": [mock_spotify_track]
+        })
 
-        # TODO: use spotify_client to check if new playlist exists
-        pass
+        mock_spotify_playlist = MagicMock(name="Mock Spotify Playlist")
+        self.mock_spotify_client.configure_mock(**{
+            "create_playlist.return_value": mock_spotify_playlist
+        })
 
-    async def test_get_tracks(self):
-        song_name = "Redbone"
-        tracks = await self.song_scrounger._get_tracks([song_name])
+        self.mock_spotify_client.configure_mock(**{
+            "add_tracks.return_value": mock_spotify_playlist
+        })
 
-        self.assertGreater(
-            len(tracks),
-            0,
-            f"Did not find any results for song '{song_name}'.",
-        )
+        playlist = await self.song_scrounger.create_playlist("mock file path", "Mock Playlist Name")
+
+        mock_read_file_contents.assert_called_once_with("mock file path")
+        mock_find_quoted_tokens.assert_called_once_with("Mock corpus with \"Mock Track Name\"")
+
+
+    async def test_get_tracks__single_track(self):
+        mock_spotify_track = MagicMock(name="Mock Spotify Track")
+        self.mock_spotify_client.configure_mock(**{
+            "find_track.return_value": [mock_spotify_track]
+        })
+        tracks = await self.song_scrounger._get_tracks(["Mock Song"])
+
+        self.mock_spotify_client.find_track.assert_called_once_with("Mock Song")
+        self.assertEqual(len(tracks), 1, "Expected exactly 1 result.")
+
+    async def test_get_tracks__multiple_tracks(self):
+        mock_spotify_track = MagicMock(name="Mock Spotify Track")
+        self.mock_spotify_client.configure_mock(**{
+            "find_track.return_value": [mock_spotify_track]
+        })
+        tracks = await self.song_scrounger._get_tracks(["Mock Song 1", "Mock Song 2", "Mock Song 3"])
+
+        self.mock_spotify_client.find_track.assert_any_call("Mock Song 1")
+        self.mock_spotify_client.find_track.assert_any_call("Mock Song 2")
+        self.mock_spotify_client.find_track.assert_any_call("Mock Song 3")
+        self.assertEqual(len(tracks), 3, "Expected exactly 3 results.")
