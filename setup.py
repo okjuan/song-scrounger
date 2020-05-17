1d7588179a35fa5e977951a8ef54beb6f0a2f906
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2020-05-17 13:57:35 -0700

    Upload working package

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Song Scrounger",
-    version="0.1.1111",
+    name="songscrounger",
+    version="0.0.11",
     author="Juan Carlos Gallegos Dupuis",
     author_email="jcgallegdup@gmail.com",
-    description="A tool for finding song names from text and creating Spotify playlists out of them.",
+    description="A tool for finding song names in text and creating Spotify playlists out of them.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/okjuan/song-scrounger",
