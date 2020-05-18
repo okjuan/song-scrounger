465e9b2e7489664568d81b8385397cec27c9931e
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2020-05-17 22:24:25 -0700

    Skip Duplicate Songs (#4)
    * Refine def'n of document parser functionality
    
    * Create test helper for test input files
    
    * Skip duplicate tracks
    
    * Add end-to-end integration test
    
    * Use Arrange-Act-Assert test pattern

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,20 +1,18 @@
 import os
 import unittest
 
+from tests import helper
+
 from song_scrounger import util
 
 class TestUtil(unittest.TestCase):
-    @classmethod
-    def _get_path_to_test_input_file(cls, name):
-        # Relative from repo root
-        return os.path.abspath(f"tests/test_inputs/{name}")
-
     def test_read_file_contents(self):
-        input_file = TestUtil._get_path_to_test_input_file("test.txt")
+        input_file = helper.get_path_to_test_input_file("test_mini.txt")
+
         contents = util.read_file_contents(input_file)
-        num_chars_read = len(contents)
+
         self.assertEqual(
-            num_chars_read,
-            19973,
-            f"Expected to read 19973 chars but read {num_chars_read} chars instead.",
+            "When Don McClean recorded \"American Pie\"\n",
+            contents,
+            "Uexpected file contents!"
         )
