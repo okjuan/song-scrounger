6e2822309ee6238894e71f8189baca832b697904
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2020-05-22 14:51:05 -0700

    Use 'song' instead of 'track' where possible

--- song_scrounger/song_scrounger.py
+++ song_scrounger/song_scrounger.py
@@ -11,49 +11,49 @@ class SongScrounger:
         self.spotify_client = spotify_client
         self.document_parser = DocumentParser(self.spotify_client)
 
-    async def create_playlist(self, name, track_names):
+    async def create_playlist(self, name, song_names):
         """Creates Spotify playlist.
 
         Params:
             name (str): name for spotify playlist.
-            track_names ([str]): e.g. ["Redbone", "Get You"].
+            song_names ([str]): e.g. ["Redbone", "Get You"].
         """
-        tracks = await self._get_tracks(track_names)
+        songs = await self._get_songs(song_names)
         playlist = await self.spotify_client.create_playlist(name)
-        return await self.spotify_client.add_tracks(playlist, tracks)
+        return await self.spotify_client.add_tracks(playlist, songs)
 
-    async def _get_tracks(self, names):
-        tracks = []
+    async def _get_songs(self, names):
+        songs = []
         for name in names:
             if len(name) == 0:
-                print(f"WARN: Skipping empty track name.")
+                print(f"WARN: Skipping empty song name.")
                 continue
 
             results = await self.spotify_client.find_track(name)
-            track = results[0] if len(results) > 0 else None
-            if track is None:
+            song = results[0] if len(results) > 0 else None
+            if song is None:
                 print(f"ERROR: Could not find song with name '{name}'")
                 continue
 
-            tracks.append(track)
-        return tracks
+            songs.append(song)
+        return songs
 
-    def parse_tracks(self, input_file_path):
-        """Finds tracks in given input file.
+    def find_songs(self, input_file_path):
+        """Finds spotify songs based on song names in given input file.
 
         - Removes duplicates: only the first occurrence of each token is kept.
         - Apart from duplicates, preserves order of tokens
-        - Strips whitespace and commas from beginning and end of each track name
+        - Strips whitespace and commas from beginning and end of each song name
 
         Params:
-            input_file_path (str): path to file containing text with track names.
+            input_file_path (str): path to file containing text with song names.
 
         Returns:
-            (str generator): sequence of track names such as "Redbone".
+            (str generator): sequence of song names such as "Redbone".
         """
         text = read_file_contents(input_file_path)
-        track_names = self.document_parser.find_quoted_tokens(text)
-        track_names = map(lambda track_name: track_name.strip(" ,"), track_names)
+        song_names = self.document_parser.find_quoted_tokens(text)
+        song_names = map(lambda song_name: song_name.strip(" ,"), song_names)
 
         def remove_dups(items):
             seen_already = set()
@@ -61,6 +61,6 @@ class SongScrounger:
                 if item not in seen_already:
                     seen_already.add(item)
                     yield item
-        unique_track_names = remove_dups(track_names)
+        unique_song_names = remove_dups(song_names)
 
-        return unique_track_names
+        return unique_song_names
--- tests/test_song_scrounger.py
+++ tests/test_song_scrounger.py
@@ -23,49 +23,49 @@ class TestSongScrounger(unittest.IsolatedAsyncioTestCase):
         self.mock_spotify_client.configure_mock(**{
             "add_tracks.return_value": "Mock Playlist Name"
         })
-        self.song_scrounger._get_tracks = AsyncMock(return_value=["Mock Spotify Track"])
+        self.song_scrounger._get_songs = AsyncMock(return_value=["Mock Spotify Track"])
 
-        playlist = await self.song_scrounger.create_playlist("Mock Playlist Name", ["mock track"])
+        playlist = await self.song_scrounger.create_playlist("Mock Playlist Name", ["mock song"])
 
         self.mock_spotify_client.create_playlist.assert_called_once_with("Mock Playlist Name")
         self.mock_spotify_client.add_tracks.assert_called_once_with("Mock Playlist Name", ["Mock Spotify Track"])
 
-    async def test_find_tracks(self):
+    async def test_get_songs(self):
         self.mock_spotify_client.configure_mock(**{
             "find_track.return_value": ["Mock Spotify Track"]
         })
 
-        playlist = await self.song_scrounger._get_tracks(["mock track"])
+        playlist = await self.song_scrounger._get_songs(["mock song"])
 
-        self.mock_spotify_client.find_track.assert_called_once_with("mock track")
+        self.mock_spotify_client.find_track.assert_called_once_with("mock song")
 
-    async def test_get_tracks__single_track(self):
+    async def test_get_songs__single_track(self):
         mock_spotify_track = MagicMock(name="Mock Spotify Track")
         self.mock_spotify_client.configure_mock(**{
             "find_track.return_value": [mock_spotify_track]
         })
         mock_song = ["Mock Song"]
 
-        tracks = await self.song_scrounger._get_tracks(mock_song)
+        tracks = await self.song_scrounger._get_songs(mock_song)
 
         self.mock_spotify_client.find_track.assert_called_once_with("Mock Song")
         self.assertEqual(len(tracks), 1, "Expected exactly 1 result.")
 
     @patch("song_scrounger.song_scrounger.read_file_contents", return_value="Once \"Redbone\", twice \"Redbone\"")
-    async def test_parse_tracks__no_dups(self, mock_read_file_contents):
-        tracks = self.song_scrounger.parse_tracks("mock file path")
+    async def test_find_songs__no_dups(self, mock_read_file_contents):
+        tracks = self.song_scrounger.find_songs("mock file path")
 
         mock_read_file_contents.assert_called_once_with("mock file path")
         self.assertEqual(list(tracks), ["Redbone"], "Expected only one result")
 
-    async def test_get_tracks__multiple_tracks(self):
+    async def test_get_songs__multiple_tracks(self):
         mock_spotify_track = MagicMock(name="Mock Spotify Track")
         self.mock_spotify_client.configure_mock(**{
             "find_track.return_value": [mock_spotify_track]
         })
         mock_tracks = ["Mock Song 1", "Mock Song 2", "Mock Song 3"]
 
-        tracks = await self.song_scrounger._get_tracks(mock_tracks)
+        tracks = await self.song_scrounger._get_songs(mock_tracks)
 
         self.mock_spotify_client.find_track.assert_any_call("Mock Song 1")
         self.mock_spotify_client.find_track.assert_any_call("Mock Song 2")
@@ -95,6 +95,6 @@ class TestSongScrounger(unittest.IsolatedAsyncioTestCase):
 
         song_scrounger = SongScrounger(spotify_client)
         input_file_path = helper.get_path_to_test_input_file(input_file_name)
-        tracks = song_scrounger.parse_tracks(input_file_path)
+        songs = song_scrounger.find_songs(input_file_path)
 
-        await song_scrounger.create_playlist(playlist_name, tracks)
+        await song_scrounger.create_playlist(playlist_name, songs)
