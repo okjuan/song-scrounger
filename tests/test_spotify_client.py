ee45035fdbff32c581e33e35cd7279054bec2936
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2020-05-25 12:35:24 -0700

    Test Spotify Client with mock HTTP calls

--- tests/test_spotify_client.py
+++ tests/test_spotify_client.py
@@ -2,11 +2,13 @@ import asyncio
 import os
 import unittest
 
+from unittest.mock import AsyncMock, MagicMock, patch
+
+from tests.helper import mock_spotify_track_factory, mock_spotify_artist_factory, get_num_times_called
 from song_scrounger.spotify_client import SpotifyClient
 from song_scrounger.util import get_spotify_creds, get_spotify_bearer_token
 
 
-@unittest.skip("Skipping integration tests by default.")
 class TestSpotifyClient(unittest.IsolatedAsyncioTestCase):
     @classmethod
     def setUpClass(cls):
@@ -16,18 +18,74 @@ class TestSpotifyClient(unittest.IsolatedAsyncioTestCase):
         bearer_token = get_spotify_bearer_token()
         cls.spotify_client = SpotifyClient(client_id, secret_key, bearer_token)
 
-    async def test_find_track__exact_match(self):
-        track = "Redbone"
+    def _get_inner_client(self, client_ctor):
+        return client_ctor.return_value.__aenter__.return_value
+
+    @patch("song_scrounger.spotify_client.Client")
+    async def test_find_track__exact_match(self, mock_inner_client_ctor):
+        track = "Mock Track"
+        mock_spotify_tracks = [mock_spotify_track_factory(
+            "Mock Track",
+            "Mock URI",
+            [mock_spotify_artist_factory("Mock Artist")]
+        )]
+        inner_cli = self._get_inner_client(mock_inner_client_ctor)
+        inner_cli.search = AsyncMock(return_value=MagicMock(tracks=mock_spotify_tracks))
 
         results = await self.spotify_client.find_track(track)
 
-        self.assertGreater(len(results), 0, "Expected to find at least one match.")
-        for result in results:
-            self.assertEqual(
-                result.name.lower(),
-                "redbone",
-                "At least one result does not match song name exactly."
+        self.assertEqual(get_num_times_called(mock_inner_client_ctor), 1)
+        inner_cli.search.assert_called_once_with("Mock Track", types=["track"])
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].name, "Mock Track")
+        self.assertEqual(results[0].uri, "Mock URI")
+        self.assertEqual(len(results[0].artists), 1)
+        self.assertEqual(results[0].artists[0].name, "Mock Artist")
+
+    @patch("song_scrounger.spotify_client.Client")
+    async def test_find_track__ignores_partial_matches(self, mock_inner_client_ctor):
+        track = "Mock Track"
+        mock_spotify_tracks = [mock_spotify_track_factory(
+            "Mock Track Partial Match",
+            "Mock URI",
+            [mock_spotify_artist_factory("Mock Artist")]
+        )]
+        inner_cli = self._get_inner_client(mock_inner_client_ctor)
+        inner_cli.search = AsyncMock(return_value=MagicMock(tracks=mock_spotify_tracks))
+
+        results = await self.spotify_client.find_track(track)
+
+        self.assertEqual(get_num_times_called(mock_inner_client_ctor), 1)
+        inner_cli.search.assert_called_once_with("Mock Track", types=["track"])
+        self.assertEqual(len(results), 0)
+
+    @patch("song_scrounger.spotify_client.Client")
+    async def test_find_track__exact_and_partial_matches__keeps_exact_match_and_skips_partial_match(
+        self, mock_inner_client_ctor
+    ):
+        track = "Mock Track Exact Match"
+        mock_spotify_tracks = [
+            mock_spotify_track_factory(
+                "Mock Track Exact Match",
+                "Mock URI",
+                [mock_spotify_artist_factory("Mock Artist")]
+            ),
+            mock_spotify_track_factory(
+                "Mock Track Partial Match",
+                "Mock URI",
+                [mock_spotify_artist_factory("Mock Artist")]
             )
+        ]
+        inner_cli = self._get_inner_client(mock_inner_client_ctor)
+        inner_cli.search = AsyncMock(return_value=MagicMock(tracks=mock_spotify_tracks))
+
+        results = await self.spotify_client.find_track(track)
+
+        self.assertEqual(len(results), 1)
+        self.assertEqual(results[0].name, "Mock Track Exact Match")
+        self.assertEqual(results[0].uri, "Mock URI")
+        self.assertEqual(len(results[0].artists), 1)
+        self.assertEqual(results[0].artists[0].name, "Mock Artist")
 
     async def test_find_track__empty_track_name__raises_value_error(self):
         track = ""
@@ -35,13 +93,7 @@ class TestSpotifyClient(unittest.IsolatedAsyncioTestCase):
         with self.assertRaises(ValueError):
             results = await self.spotify_client.find_track(track)
 
-    async def test_find_track__when_no_exact_match__returns_empty(self):
-        track = "nofasdflkdnfasfdbaskdfjabsdfjlkasfd"
-
-        results = await self.spotify_client.find_track(track)
-
-        self.assertEqual([], results, "Should not find any results")
-
+    @unittest.skip("Integration tests disabled by default.")
     async def test_create_playlist(self):
         name = f"DELETE ME: test_create_playlist in song_scrounger"
         spotify_uris = [
@@ -53,6 +105,7 @@ class TestSpotifyClient(unittest.IsolatedAsyncioTestCase):
 
         self.assertIsNotNone(playlist, "Playlist creation failed: received 'None' as result")
 
+    @unittest.skip("Integration tests disabled by default.")
     async def test_create_empty_playlist(self):
         name = f"DELETE ME: test_create_empty_playlist in song_scrounger"
 
@@ -60,6 +113,7 @@ class TestSpotifyClient(unittest.IsolatedAsyncioTestCase):
 
         self.assertIsNotNone(playlist, "Playlist creation failed: received 'None' as result")
 
+    @unittest.skip("Integration tests disabled by default.")
     async def test_add_tracks(self):
         # Named 'Song Scrounger Test Playlist' on Spotify
         playlist_id = "spotify:playlist:1mWKdYnyaejjLrdK7pBg2K"
